7f53d53f7da8a80809c2d06eed6589b11c8dca14
Kurapov75 <kurapov1975@gmail.com>
2019-09-22 13:23:50 +1100

    Создан файл stat.js и подключен к index.html. Реализована отрисовка окна по завершению игры.

---
+++ js/stat.js
@@ -0,0 +1,16 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function(ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!\nСписок результатов:', 140, 50);
+};
